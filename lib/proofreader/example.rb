08d8207b5565a3a4e5d6df200b215624e40a01f1
Rajas Pargaonkar <rajas@waddleworks.com>
2019-05-15 21:25:37 -0400

    Updated all classes with optional attributes and nested elements outside of those nested inside of pattern. Only one complete in pattern is token.

--- lib/proofreader/example.rb
+++ lib/proofreader/example.rb
@@ -14,21 +14,6 @@ class Proofreader
         example: example_xml.text
       )
     end
-
-    # def format_example(raw_examples)
-    #   examples = []
-      
-    #   example_with_correction = raw_examples.select { |example| example.attributes['correction'] }[0]
-    #   marker = example_with_correction.children.select { |child| child.name == 'marker' }[0].text
-    #   sample_wrong_string = example_with_correction.children.text
-    #   alternate_tokens = example_with_correction.attributes["correction"].value.split('|')
-
-    #   alternate_tokens.length.times.with_index do |i|
-    #     examples << sample_wrong_string.gsub("#{marker}", alternate_tokens[i])
-    #   end
-
-    #   examples
-    # end
   end
 end
 
